a2c008ef8d7028dadbef5eda0e943a58640ec001
Angelo + Ke <angelo+ke@appacademy.io>
2013-05-23 14:53:29 -0400

    ship can be hit

--- asteroids.js
+++ asteroids.js
@@ -7,15 +7,17 @@ function Asteroid(pos, vel) {
 function Game(ctx) {
   this.ctx = ctx;
   this.asteroids = [];
-  for(var i = 0; i < 10; i++) {
+  for(var i = 0; i < 50; i++) {
     this.asteroids.push(Asteroid.randomAsteroid());
   }
+  this.ship = new Ship({x: 300, y: 300}, this);
 
   this.draw = function() {
     this.ctx.clearRect(0, 0, 600, 600);
     for (var i = 0, len = this.asteroids.length; i < len; ++i) {
-      this.asteroids[i].draw(ctx);
+      this.asteroids[i].draw(this.ctx);
     }
+    this.ship.draw(this.ctx);
   };
 }
 
@@ -31,9 +33,17 @@ var randomNum = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+var distance_squared = function(pos1, pos2) {
+  var dx = pos1.x - pos2.x
+  var dy = pos1.y - pos2.y
+
+  return dx * dx + dy * dy;
+};
+
 Asteroid.prototype.draw = function(ctx) {
   ctx.beginPath();
   ctx.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * 2, true);
+  ctx.fillStyle = "black";
   ctx.fill();
 };
 
@@ -51,13 +61,42 @@ Game.prototype.update = function() {
       this.asteroids.splice(i--, 1);
     }
   }
-};
 
+  if (this.ship.isHit()) {
+    alert("Game over");
+    clearInterval(this.interval);
+  }
+};
 
 Game.prototype.start = function() {
   var that = this;
-  setInterval(function() {
+  this.interval = setInterval(function() {
     that.update();
     that.draw();
   }, 32);
+};
+
+function Ship(pos, game) {
+  this.pos = pos;
+  this.r = 10;
+  this.game = game;
+}
+
+Ship.prototype.draw = function(ctx) {
+  ctx.beginPath();
+  ctx.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI*2,true);
+  ctx.fillStyle = "red";
+  ctx.fill();
+};
+
+Ship.prototype.isHit = function() {
+  var asteroids = this.game.asteroids;
+  for (var i = 0; i < asteroids.length; ++i) {
+    var ast = asteroids[i];
+    if (distance_squared(this.pos, ast.pos) < Math.pow(this.r + ast.r, 2)) {
+      return true;
+    }
+  }
+
+  return false;
 };
