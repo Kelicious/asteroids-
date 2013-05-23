9bc316c44c08cde26c0c756e423ba006e1d7f750
Angelo + Ke <angelo+ke@appacademy.io>
2013-05-23 13:41:25 -0400

    phase1 asteroids can animate and game can delete asteroids

--- asteroids.js
+++ asteroids.js
@@ -1,4 +1,5 @@
-function Asteroid(pos) {
+function Asteroid(pos, vel) {
+  this.vel = vel;
   this.pos = pos;
   this.r = 10;
 }
@@ -11,6 +12,7 @@ function Game(ctx) {
   }
 
   this.draw = function() {
+    this.ctx.clearRect(0, 0, 600, 600);
     for (var i = 0, len = this.asteroids.length; i < len; ++i) {
       this.asteroids[i].draw(ctx);
     }
@@ -20,19 +22,42 @@ function Game(ctx) {
 Asteroid.randomAsteroid = function() {
   var x = Math.floor(Math.random()*600);
   var y = Math.floor(Math.random()*600);
-  return new Asteroid([x,y]);
+  var dx = randomNum(-10,10);
+  var dy = randomNum(-10,10);
+  return new Asteroid({ x: x, y: y }, {dx: dx, dy: dy});
 };
 
-Asteroid.prototype.x = function() {
-  return this.pos[0];
-};
-
-Asteroid.prototype.y = function() {
-  return this.pos[1];
+var randomNum = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 Asteroid.prototype.draw = function(ctx) {
   ctx.beginPath();
-  ctx.arc(this.x(), this.y(), this.r, 0, Math.PI * 2, true);
+  ctx.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * 2, true);
   ctx.fill();
+};
+
+Asteroid.prototype.update = function() {
+  this.pos.x += this.vel.dx;
+  this.pos.y += this.vel.dy;
+};
+
+Game.prototype.update = function() {
+  for (var i = 0; i < this.asteroids.length; ++i) {
+    var ast = this.asteroids[i];
+    ast.update();
+    if ((ast.pos.x > 600 || ast.pos.x < 0) ||
+    (ast.pos.y > 600 || ast.pos.y < 0)) {
+      this.asteroids.splice(i--, 1);
+    }
+  }
+};
+
+
+Game.prototype.start = function() {
+  var that = this;
+  setInterval(function() {
+    that.update();
+    that.draw();
+  }, 32);
 };
